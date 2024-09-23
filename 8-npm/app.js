58e50fc8391bfa6756deb709ba678079918cc1d7
Muhammad Faizal Addi <faizal.addi@gmail.com>
2024-09-23 20:24:19 +0800

    Learn chalk and validator module

---
+++ 8-npm/app.js
@@ -0,0 +1,11 @@
+import chalk from 'chalk';
+
+console.log(chalk.blue('Hello world!'));
+// const validator = require('validator');
+
+
+// console.log(validator.isEmail('Faizal.addi@gmail.com'))
+// console.log(validator.isMobilePhone('085240451633'), 'id-ID')
+// console.log(validator.isNumeric('0s85240451633'))
+
+console.log(chalk.black.bgBlue('Hello World'))
