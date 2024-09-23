395f480c20c60b2a15bfb754f37cfd956a250ab3
Muhammad Faizal Addi <faizal.addi@gmail.com>
2024-09-23 12:04:55 +0800

    Learning module system in node JS
    Belajar export dan require module local di node js

--- coba.js
+++ coba.js
@@ -2,4 +2,35 @@ function cetakNama(nama){
     return `Halo nama saya ${nama}`
 }
 
-module.exports = cetakNama;
+const PI= 3.14;
+
+const mahasiswa = {
+    nama : 'Dody',
+    umur : 20,
+    cetakMahasiswa(){
+        return `halo nama saya ${this.nama} berusian ${this.umur} tahun`
+    }
+}
+
+class Orang{
+    constructor(){
+        console.log('object orang telah dibuat');
+    }
+}
+
+// Exports module satu persatu
+// module.exports.cetakNama = cetakNama;
+// module.exports.PI = PI;
+// module.exports.mahasiswa = mahasiswa;
+// module.exports.Orang = Orang;
+
+// Export sekaligus menggunakan object
+// module.exports = {
+//     cetakNama : cetakNama,
+//     PI : PI,
+//     mahasiswa: mahasiswa,
+//     Orang : Orang
+// }
+
+// Export sekaligus menggunakan notasi ES6
+module.exports = {cetakNama, PI, mahasiswa, Orang}
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-// console.log('hello wpu')
-// const nama = 'faizal';
-// const cetakNama = (nama) => `Hi nama saya ${nama}`;
-// console.log(cetakNama(nama));
+// const fs = require('fs'); // Core module
+// const cetakNama = require ('./coba'); // import local module
+// const moment = require('moment'); // thir party module / npm module akan ada di dalam folder node_modules
 
-const cetakNama = require ('./coba');
+// const cetakNama = require('./coba');
+// const PI = require('./coba')
 
-console.log(cetakNama('sandhika')
-)
+const coba = require('./coba');
+
+
+console.log(coba.cetakNama('Faizal'), coba.PI, coba.mahasiswa.cetakMahasiswa(), new coba.Orang());
