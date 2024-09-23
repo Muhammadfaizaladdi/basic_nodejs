11ac47c6c230651a43b5f19a4223cfd862dc9924
Muhammad Faizal Addi <faizal.addi@gmail.com>
2024-09-23 11:44:24 +0800

    Hello Node JS
    First commit in node js journey

---
+++ index.js
@@ -0,0 +1,9 @@
+// console.log('hello wpu')
+// const nama = 'faizal';
+// const cetakNama = (nama) => `Hi nama saya ${nama}`;
+// console.log(cetakNama(nama));
+
+const cetakNama = require ('./coba');
+
+console.log(cetakNama('sandhika')
+)
